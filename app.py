8834f406ed016890c5bbf96a5deb0c6f5d0aeeb7
Soykat Amin <soykatamin@gmail.com>
2024-04-03 11:58:07 +0200

    Created a bunch of files and folders to start the project

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///MindMasters.db'
+db = SQLAlchemy(app)
+
+class MindMasters(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False)
+    message = db.Column(db.String(500), nullable=False)
+
+    def __repr__(self):
+        return '<MindMasters %r>' % self.name
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
