6c3ab0399efd72c724d571513b60cddedfb33377
Soykat Amin <soykatamin@gmail.com>
2024-04-08 12:18:45 +0200

    Login and register done. Both frontend and backend

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, request, redirect, url_for, session
 from flask_sqlalchemy import SQLAlchemy
+import bcrypt 
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your secret key'
 
 # DATABASE CONFIGURATION
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///MindMasters.db'
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+        'sqlite:///' + os.path.join(basedir, 'database.db')
 db = SQLAlchemy(app)
 
 class User(db.Model):
@@ -18,6 +24,9 @@ class User(db.Model):
     is_admin = db.Column(db.Boolean, default=False)
 
     statistics = db.relationship("Statistic", backref="user", uselist=False)  # One-to-One with Statistic
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 class Game(db.Model):
     __tablename__ = 'games'
@@ -40,28 +49,70 @@ class Statistic(db.Model):
     losses = db.Column(db.Integer, nullable=False, default=0)
     first_try_wins = db.Column(db.Integer, nullable=False, default=0)
 
-
-
 # Create tables in database (if not exist)
 with app.app_context():
     db.create_all()
 
 @app.route('/', methods=['GET', 'POST'])
+
 def index():
     return render_template('index.html')
 
 @app.route('/register', methods=['GET', 'POST'])
+
 def register():
+    msg = ''
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+
+        if username in [user.username for user in User.query.all()]:
+            msg = 'Username already exists'
+            return render_template('auth/register.html', msg=msg)
+        
+        # Save hashed password to database for security purposes
+        salt = bcrypt.gensalt() 
+        hashed_password = bcrypt.hashpw(password.encode('utf-8'), bytes(salt))
+        new_user = User(username=username, password=hashed_password, email=email)
+        db.session.add(new_user)
+        db.session.commit()
+
+        return redirect(url_for('login'))
     return render_template('auth/register.html')
 
 @app.route('/login', methods=['GET', 'POST'])
+
 def login():
-    return render_template('auth/login.html')
+    msg = ''
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        user = User.query.filter_by(username=username).first()
+        print(user)
+
+        if user:
+            session['username'] = username # Store username in session
+            if user.password == bcrypt.hashpw(password.encode('utf-8'), user.password):
+                return redirect(url_for('index'))
+            else:
+                msg = 'Incorrect password'
+        else:
+            msg = 'Username does not exist'
+
+    return render_template('auth/login.html', msg=msg)
+
+@app.route('/logout')
+
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('index'))
 
 @app.route('/regole', methods=['GET', 'POST'])
+
 def rules():
     return render_template('rules.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
-
