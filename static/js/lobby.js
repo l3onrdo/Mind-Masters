a2db9b39482503d1c92d77b235bd62cc42472e76
l3onrdo <leomiralli@gmail.com>
2024-04-19 22:18:51 +0200

    Improved the phrase randomizer in the lobby

--- static/js/lobby.js
+++ static/js/lobby.js
@@ -1,26 +1,37 @@
-var frasi=["Senza matematica; ma dove siamo?","SI È MESSO A FASE SELECTION SORTTT!!!","Nel complesso, sai cheee", "1982783 è il tag del miglior giocatore!"];
+var frai=["Senza matematica; ma dove siamo?","SI È MESSO A FASE SELECTION SORTTT!!!","Nel complesso, sai cheee", "Il sito si trova alla versione: 1982783","Sapevi che i numeri da 1 a 8 da tasiera ti permettono di inserire i colori "];
+var frasi=["1111111","222222222","333333333"]
 var frasiUsate = [];
+var ultimaFrase = "";
 
 function rotazione(){
     randomFrasi();
-    //imposta una pausa di 2 secondi
     setTimeout(rotazione, 6999);
 }
 
 function randomFrasi(){
+    
     var frasiDisponibili = frasi.filter(function(frase) {
         return !frasiUsate.includes(frase);
     });
-
+    
     if (frasiDisponibili.length === 0) {
-        // All phrases have been used, reset the used phrases array
         frasiUsate = [];
         frasiDisponibili = frasi;
-    }
+        //rimuovi dall array la frase usata per ultima 
+        frasiDisponibili = frasiDisponibili.filter(function(frase) {
+            return frase !== ultimaFrase;
+        });
 
+        
+    }
+    console.log(frasiDisponibili);
+    if(frasiDisponibili.length === 1 ){
+        ultimaFrase = frasiDisponibili[0];
+    }
+    
     var random = Math.floor(Math.random() * frasiDisponibili.length);
     var frase = frasiDisponibili[random];
     frasiUsate.push(frase);
-
+   
     document.getElementById("frasi").innerHTML = frase;
 }
