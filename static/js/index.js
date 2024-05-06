3763a02eec6d2453e348e0d1c1e83bcced2eac05
Soykat Amin <soykatamin@gmail.com>
2024-05-06 23:32:42 +0200

    Changed sidebar. Now it's fixed. Still have to resolve initial animation bug.

--- static/js/index.js
+++ static/js/index.js
@@ -3,59 +3,38 @@
 document.addEventListener("DOMContentLoaded", function() {
     // ---------------------------------------------MANAGING SIDEBAR--------------------------------------------- // 
     
-    toggle = true;
-    let sidebarOpen = true;
-    let sidebarClosed = false;
     screenWidth = window.innerWidth;
-    
  
     // Fuction for the sidebar toggle button
     toggleSidebarButton.addEventListener('click', function toggleSidebar() {
         // Toggle of class 'collapsed' of sidebar
-        if(screenWidth <= screenLimit){
-            toggleSidebarButton.style.visibility = 'hidden';
-            closeSidebarButton.style.visibility = 'visible';
-        }else{
-            closeSidebarButton.style.visibility = 'hidden';
-        }
         
-        if (sidebarOpen) {
-            sidebar.classList.toggle('closed');
-            sidebarOpen = false;
-            sidebarClosed = true;
+        toggleSidebarButton.style.visibility = 'hidden';
+        closeSidebarButton.style.visibility = 'visible';
+        isSidebarClosed = localStorage.getItem('sidebarCollapsed') === 'true';
+        if (isSidebarClosed) {
+            openSidebar();
         } else {
-            sidebar.classList.toggle('closed');
-            sidebarOpen = true;
-            sidebarClosed = false;
+            closeSidebar();
         }
-
-        toggle = !toggle;
-
-        //console.log("toggle: " + toggle);
-
-        // Save the state of the sidebar in the local storage
-        localStorage.setItem('sidebarCollapsed', sidebar.classList.contains('closed'));
     });
 
     // Function for the close sidebar button
-    closeSidebarButton.addEventListener('click', function closeSidebar() {
-        if(screenWidth <= screenLimit){
-            toggleSidebarButton.style.visibility = 'visible';
-            closeSidebarButton.style.visibility = 'hidden';
+    closeSidebarButton.addEventListener('click', function closeSidebarBtn() {
+        isSidebarClosed = localStorage.getItem('sidebarCollapsed') === 'true';
+        if (!isSidebarClosed) {
+            closeSidebar();
         }
-        sidebar.classList.add('closed');
-        localStorage.setItem('sidebarCollapsed', sidebar.classList.contains('closed'));
     });
     
     // Function to manage the sidebar state on page load
     window.addEventListener('load', function() {
-
-        const isSidebarCollapsed = localStorage.getItem('sidebarCollapsed') === 'true';
-        if (window.innerWidth <= screenLimit) {
-            sidebar.classList.add('closed');
-            localStorage.setItem('sidebarCollapsed', 'true');
-        }else {
-            closeSidebarButton.style.visibility = 'hidden';
+        isSidebarClosed = localStorage.getItem('sidebarCollapsed') === 'true';
+        if (isSidebarClosed) {
+            openSidebar();
+        }
+        else{
+            closeSidebar();
         }
     });
 
@@ -63,34 +42,24 @@ document.addEventListener("DOMContentLoaded", function() {
     window.addEventListener('resize', function() {
 
         screenWidth = window.innerWidth;
-        
-        const isSidebarCollapsed = localStorage.getItem('sidebarCollapsed') === 'true';
+        isSidebarClosed = localStorage.getItem('sidebarCollapsed') === 'true';
         // console.log(isSidebarCollapsed);
         if (window.innerWidth <= screenLimit) {
             // If the window is smaller than screenLimit
-            closeSidebarButton.style.visibility = 'visible';
-            if (!isSidebarCollapsed) {
-                // If the sidebar is open, close it and save the state
-                
-                toggleSidebarButton.style.visibility = 'visible';
-                sidebar.classList.add('closed');
-                localStorage.setItem('sidebarCollapsed', 'true');
+            if (!isSidebarClosed) {
+                closeSidebar();
             }
         } else {
             // If the window is larger than screenLimit
-            if (isSidebarCollapsed && toggle) {
+            if (isSidebarClosed && toggle) {
                 // If the sidebar is closed and the user has manually opened it, reopen it and save the state
-                closeSidebarButton.style.visibility = 'hidden';
-                toggleSidebarButton.style.visibility = 'visible';
-                sidebar.classList.remove('closed');
-                localStorage.setItem('sidebarCollapsed', 'false');
+                openSidebar();
             }
         }
     });
 
     // ---------------------------------------------END MANAGING SIDEBAR--------------------------------------------- // 
     //-------gestione accessibilita-----//
-   
 
 });
 
